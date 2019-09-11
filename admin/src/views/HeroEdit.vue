9a15ddaedd3f676c0237ca27a0072d6fb9979795
huangxj <huangxiujie@email.com>
2019-09-12 00:12:52 +0800

    编辑英雄

---
+++ admin/src/views/HeroEdit.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="about">
+        <h1>{{ id ? '编辑' : '新建' }}英雄</h1>
+        <el-form label-width="120px" @submit.native.prevent="save">
+            <el-form-item label="名称">
+                <el-input v-model="model.name"></el-input>
+            </el-form-item>
+            <el-form-item label="称号">
+                <el-input v-model="model.title"></el-input>
+            </el-form-item>
+            <el-form-item label="类型">
+                <el-select v-model="model.category" multiple>
+                    <el-option v-for="item of categories" :key="item._id"
+                    :label="item.name"
+                    :value="item._id"
+                    ></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="难度">
+                <el-rate style="margin-top:0.6rem;" show-score :max="9" v-model="model.scores.difficult"></el-rate>
+            </el-form-item>
+            <el-form-item label="技能">
+                <el-rate style="margin-top:0.6rem;" show-score :max="9" v-model="model.scores.skills"></el-rate>
+            </el-form-item>
+            <el-form-item label="攻击">
+                <el-rate style="margin-top:0.6rem;" show-score :max="9" v-model="model.scores.attack"></el-rate>
+            </el-form-item>
+            <el-form-item label="生存">
+                <el-rate style="margin-top:0.6rem;" show-score :max="9" v-model="model.scores.survive"></el-rate>
+            </el-form-item>
+            <el-form-item label="头像">
+                <el-upload
+                class="avatar-uploader"
+                :action="$http.defaults.baseURL+'/upload'"
+                :show-file-list="false"
+                :on-success="afterUpload"
+                >
+                <img v-if="model.avatar" :src="model.avatar" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>    
+                </el-upload>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" native-type="submit">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        id:{}
+    },
+    data(){
+        return {
+            categories:[],
+            model:{
+                name:'',
+                avatar:'',
+                scores:{
+                    difficult:0
+                }
+            },
+        }
+    },
+    methods:{
+        async save(){
+            if(this.id){
+                //更新
+                await this.$http.put(`rest/heroes/${this.id}`,this.model)
+                //新建
+            }else{
+                await this.$http.post('rest/heroes',this.model)
+            }
+            this.$router.push('/heroes/list')
+            this.$message({
+                type:'success',
+                message:'保存成功'
+            })
+        },
+        async fetch(){
+            const res = await this.$http.get(`rest/heroes/${this.id}`)
+            this.model = Object.assign({},this.model,res.data)
+        },
+        async fetchCategories(){
+            const res = await this.$http.get(`rest/categories`)
+            this.categories = res.data
+        },
+        afterUpload(res){
+            console.log(res);
+            // vue中响应数据的问题
+            //this.$set(this.model,'avatar',res.url)
+            this.model.avatar = res.url
+        }
+    },
+    created(){
+        this.fetchCategories()
+        this.id && this.fetch()
+    }
+}
+</script>
+
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+    vertical-align: middle;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
