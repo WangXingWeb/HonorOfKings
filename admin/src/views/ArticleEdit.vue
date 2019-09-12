5ab77cc2599ff0eb72a9e9dfcef9530bb798882c
王星 <wx1620@163.com>
2019-09-12 12:53:34 +0800

    '文章'

---
+++ admin/src/views/ArticleEdit.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="about">
+        <h1>{{ id ? '编辑' : '新建' }}文章</h1>
+        <el-form label-width="120px" @submit.native.prevent="save">
+            <el-form-item label="所属分类">
+                <el-select v-model="model.categories" multiple placeholder="请选择">
+                    <el-option
+                    v-for="item in categories"
+                    :key="item._id"
+                    :label="item.name"
+                    :value="item._id">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="标题">
+                <el-input v-model="model.title"></el-input>
+            </el-form-item>
+            <el-form-item label="详情">
+                <el-input v-model="model.body"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" native-type="submit">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        id:{}
+    },
+    data(){
+        return {
+            categories:[],
+            model:{},
+        }
+    },
+    methods:{
+        async save(){
+            if(this.id){
+                //更新
+                await this.$http.put(`rest/articles/${this.id}`,this.model)
+                //新建
+            }else{
+                await this.$http.post('rest/articles',this.model)
+            }
+            this.$router.push('/articles/list')
+                this.$message({
+                    type:'success',
+                    message:'保存成功'
+                })
+        },
+        async fetch(){
+            const res = await this.$http.get(`rest/articles/${this.id}`)
+            this.model = res.data
+        },
+        async fetchCategories(){
+            const res = await this.$http.get(`rest/categories`)
+            this.categories = res.data
+        }
+        
+    },
+    created(){
+        this.fetchCategories()
+        this.id && this.fetch()
+    }
+}
+</script>
