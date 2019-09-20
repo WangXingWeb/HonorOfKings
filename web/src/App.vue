d36bedca5417d953087e193ba325ed8bb11c905d
王星 <wx1620@163.com>
2019-09-20 16:08:06 +0800

    'web'

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>{{ text }}7777</div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  data(){
-    return {
-      text:'ddd'
-    }
-    
-  },
-  components: {
-    HelloWorld
-  }
-}
-</script>
